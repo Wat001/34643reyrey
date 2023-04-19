51df424267ee71f3c5d35563947412c6323b39f2
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-19 23:09:35 +0300

    Add files via upload

--- script.py
+++ script.py
@@ -1,24 +1,13 @@
-from selenium import webdriver
-import time
-from plyer import notification
-import winsound
-
-url = "https://www.roblox.com/groups/3576655/Zombie-Attack-Official#!/about"
-text_to_check= "emir"
-
-driver = webdriver.Chrome()
+driver.get(url)
 
 while True:
-    driver.get(url)
-    time.sleep(2)
-    
     if text_to_check in driver.page_source:
         print("bulundu!")
         
         notification.notify(
             title="bulundu!",
             message=f"yazi'{text_to_check}' bulundu{url}.",
-            app_name="Isım aratici",
+            app_name="Webpage Checker",
             timeout=10
         )
         
@@ -29,5 +18,3 @@ while True:
         break
     
     time.sleep(1)
-
-driver.quit()
