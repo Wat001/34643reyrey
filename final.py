46b34d1cc6e2a06d75aa84e0eb47d328e1689aba
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 16:44:12 +0300

    Update final.py

--- final.py
+++ final.py
@@ -3,17 +3,20 @@ from selenium import webdriver
 import time
 
 url = "https://www.roblox.com/groups/3576655/Zombie-Attack-Official#!/about"
-text_to_check = r"\b[eE][mM][iI][rR]\b"
+text_to_check = r".*\b[eE][mM][iI][rR]\b.*"
 ignore_text = "(D|d)emir"
 
 driver = webdriver.Chrome()
 driver.get(url)
 
+found_names = set()
+
 while True:
     page_source = driver.page_source
     matches = re.findall(text_to_check, page_source)
     for match in matches:
-        if not re.search(ignore_text, match):
+        if not re.search(ignore_text, match) and match not in found_names:
+            found_names.add(match)
             print(f"bulundu! '{match}'")
             
     time.sleep(0.25)
