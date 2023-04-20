c7ab0c6de6b75f20bbf151af0bf2d14c020a8269
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 15:02:15 +0300

    Update final.py

--- final.py
+++ final.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 import time
 
 url = input("Enter URL to monitor: ")
-text_to_check = "(E|e)mir"
-ignore_text = "(D|d)emir"
+text_to_check = "(E|e)mir.*"
+ignore_text = "(D|d)emir.*"
 
 driver = webdriver.Chrome()
 driver.get(url)
@@ -13,13 +13,9 @@ matched_usernames = set()
 
 while True:
     page_source = driver.page_source
-    matched_names = re.finditer(text_to_check, page_source)
-    new_names = set()
-    for name in matched_names:
-        name = name.group()
-        if not re.search(ignore_text, name):
-            if name not in matched_usernames:
-                new_names.add(name)
+    matched_names = re.findall(text_to_check, page_source)
+    filtered_names = [name for name in matched_names if not re.search(ignore_text, name)]
+    new_names = set(filtered_names) - matched_usernames
     if new_names:
         print("bulundu!")
         with open("isimler.txt", "a") as f:
