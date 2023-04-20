bdf513e6da559c15e74cafebee1adc97bfd41145
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 15:07:41 +0300

    Update final.py

--- final.py
+++ final.py
@@ -13,17 +13,15 @@ matched_usernames = set()
 
 while True:
     page_source = driver.page_source
-    matched_names = re.finditer(text_to_check, page_source)
-    new_names = set()
-    for name in matched_names:
-        name = name.group()
-        if not re.search(ignore_text, name):
-            if name not in matched_usernames:
-                new_names.add(name)
+    matched_names = re.findall(text_to_check, page_source)
+    filtered_names = [name for name in matched_names if not re.search(ignore_text, name)]
+    new_names = set(filtered_names) - matched_usernames
     if new_names:
         print("bulundu!")
         with open("isimler.txt", "a") as f:
-            f.write("\n".join(new_names) + "\n")
+            for name in new_names:
+                f.write(name + "\n")
         matched_usernames |= new_names
 
     time.sleep(0.25)
+
