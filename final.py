49165274393b603c95d83e3f1229fc4cec12a352
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 16:40:55 +0300

    Update final.py

--- final.py
+++ final.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 import time
 
 url = "https://www.roblox.com/groups/3576655/Zombie-Attack-Official#!/about"
-text_to_check = "(E|e)mir"
+text_to_check = r"\b[eE][mM][iI][rR]\b"
 ignore_text = "(D|d)emir"
 
 driver = webdriver.Chrome()
@@ -17,4 +17,3 @@ while True:
             print(f"bulundu! '{match}'")
             
     time.sleep(0.25)
-
