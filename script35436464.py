52cddfcd2fb29d60e4efb630dca2583cd151546d
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 14:37:54 +0300

    Update script35436464.py

--- script35436464.py
+++ script35436464.py
@@ -1,27 +1,19 @@
+import re
 from selenium import webdriver
 import time
-from plyer import notification
-import winsound
 
-url = "https://www.roblox.com/groups/3576655/Zombie-Attack-Official#!/about"
-text_to_check = "emir"
+url = input("Enter URL to monitor: ")
+text_to_check = "(E|e)mir"
+ignore_text = "(D|d)emir"
 
 driver = webdriver.Chrome()
 driver.get(url)
 
 while True:
-    if text_to_check in driver.page_source:
+    page_source = driver.page_source
+    if re.search(text_to_check, page_source) and not re.search(ignore_text, page_source):
         print("bulundu!")
-
-        notification.notify(
-            title="bulundu!",
-            message=f"yazi'{text_to_check}' bulundu{url}.",
-            app_name="Webpage Checker",
-            timeout=10
-        )
-
-        duration = 1000
-        frequency = 440
-        winsound.Beep(frequency, duration)
+        with open("isimler.txt", "a") as f:
+            f.write(re.findall(text_to_check, page_source)[0] + "\n")
 
     time.sleep(0.25)
