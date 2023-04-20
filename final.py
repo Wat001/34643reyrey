aaea1841e9d5d07063a867cfb1348c2c5d866d5c
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 16:06:31 +0300

    Update final.py

--- final.py
+++ final.py
@@ -1,22 +1,30 @@
 import re
 from selenium import webdriver
 import time
+from plyer import notification
+import winsound
 
-url = input("Enter URL to monitor: ")
+url = "https://www.roblox.com/groups/3576655/Zombie-Attack-Official#!/about"
 text_to_check = "(E|e)mir"
 ignore_text = "(D|d)emir"
 
 driver = webdriver.Chrome()
 driver.get(url)
 
-matched_strings = set()
-
 while True:
     page_source = driver.page_source
-    matches = re.findall(text_to_check, page_source, re.IGNORECASE)
-    for match in matches:
-        if match not in matched_strings and not re.search(ignore_text, match):
-            print(f"bulundu! {match}")
-            matched_strings.add(match)
-    time.sleep(0.25)
+    if re.search(text_to_check, page_source) and not re.search(ignore_text, page_source):
+        print("bulundu!")
 
+        notification.notify(
+            title="bulundu!",
+            message=f"yazi'{text_to_check}' bulundu{url}.",
+            app_name="Webpage Checker",
+            timeout=10
+        )
+
+        duration = 1000
+        frequency = 440
+        winsound.Beep(frequency, duration)
+
+    time.sleep(0.25)
