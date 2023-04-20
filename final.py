e587c301940da0657466c0b00d2c5c424649c98e
Wat001 <125084395+Wat001@users.noreply.github.com>
2023-04-20 15:51:10 +0300

    Update final.py

--- final.py
+++ final.py
@@ -3,26 +3,15 @@ from selenium import webdriver
 import time
 
 url = input("Enter URL to monitor: ")
-text_to_check = "^Emir.*"
-ignore_text = "(D|d)emir.*"
-output_file = input("Dosya yolu: ")
+text_to_check = "(E|e)mir"
+ignore_text = "(D|d)emir"
 
 driver = webdriver.Chrome()
 driver.get(url)
 
-matched_usernames = set()
-
 while True:
     page_source = driver.page_source
-    matched_names = re.findall(text_to_check, page_source)
-    filtered_names = [name for name in matched_names if not re.search(ignore_text, name, flags=re.IGNORECASE)]
-    new_names = set(filtered_names) - matched_usernames
-    if new_names:
-        print("Bulundu!")
-        with open(output_file, "a", encoding="utf-8") as f:
-            for name in new_names:
-                f.write(name + "\n")
-        matched_usernames |= new_names
-
+    if re.search(text_to_check, page_source) and not re.search(ignore_text, page_source):
+        print("bulundu!")
+        
     time.sleep(0.25)
-
